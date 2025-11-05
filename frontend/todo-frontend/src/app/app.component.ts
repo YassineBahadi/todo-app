88e61342908b332d2b06d71302919e02667ade36
YassineBahadi <yassinebahadi04@gmail.com>
2025-11-05 02:41:01 +0100

    fix: add @angular/animations v19.2.15 and configure provideAnimations()

--- frontend/todo-frontend/src/app/app.component.ts
+++ frontend/todo-frontend/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-frontend';
-  constructor(private router: Router,private authService:AuthService) {}
+  authService = inject(AuthService);
+  router = inject(Router);
 }
